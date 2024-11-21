95105deb6794b9ed216cb81da5aafd1a05c31106
Rouslan Gabiss <43136755+RouslanG@users.noreply.github.com>
2024-11-21 21:39:06 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,8 @@ export default withSentryConfig(
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
+    telemetry: false,
+
     // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
     // This can increase your server load as well as your hosting bill.
     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
